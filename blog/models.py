39dba67182a46edea0af925a6d2af886331195dc
李顺风 <mac2020@126.com>
2017-07-17 00:05:21 +0800

    数据库 admin

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=32,default='Title')
+    content = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 # from django.http import HttpResponse
+from . import models
 
 
 def index(request):
-    return render(request,'index.html',{'hello':'猜猜我是谁'})
+    articles = models.Article.objects.all()
+    return render(request,'index.html',{'articles':articles})
 
