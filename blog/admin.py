a5b7d5203d78de277cac3cec2ae7b8fd47879703
李顺风 <mac2020@126.com>
2017-07-19 00:30:00 +0800

    文章修改  新文章 admin增强

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 
 from blog.models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_time')
+    
+
+admin.site.register(Article,ArticleAdmin)
--- myblog/urls.py
+++ myblog/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.ulrs')),
+    url(r'^blog/', include('blog.ulrs',namespace='blog')),
 ]
