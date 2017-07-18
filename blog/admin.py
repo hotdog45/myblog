768d645c0f3f7ab7fdadd83208d33128417852dd
李顺风 <mac2020@126.com>
2017-07-19 01:16:57 +0800

    增加创建时间

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,6 @@ from blog.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title','content','pub_time')
-    
+    list_filter = ('pub_time',)
 
 admin.site.register(Article,ArticleAdmin)
--- myblog/settings.py
+++ myblog/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans' #zh_hant繁体字
+LANGUAGE_CODE = 'zh_Hans'
 
 TIME_ZONE = 'UTC'
 
